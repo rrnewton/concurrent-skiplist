c24132f4c5e21f348968ef3d00c1a3a3eeff659c
Ryan Newton <rrnewton@gmail.com>
2014-06-04 11:25:08 -0400

    Add a type-family for key constraints in the generic interface

--- concurrent-skiplist/Data/Concurrent/Map/Class.hs
+++ concurrent-skiplist/Data/Concurrent/Map/Class.hs
@@ -1,4 +1,4 @@
-
+{-# LANGUAGE TypeFamilies, ConstraintKinds #-}
 
 -- | An abstract interface to concurrent maps.
 --
@@ -7,37 +7,38 @@
 module Data.Concurrent.Map.Class where
 
 
+import GHC.Prim (Constraint)
+--------------------------------------------------------------------------------
+
+
 -- | A concurrent map that can grow but does not support deletion.
 class ConcurrentInsertMap mp where
 
-  -- | Constraints on the keys.  Implementation-specific.
---  type 
-  
+  -- | Different implementations may place different constraints on
+  -- what is required of keys: for example Eq or Hashable.
+  type Key mp k :: Constraint
+    
   -- | Creates a new concurrent-map.
   new :: IO (mp k v)
 
   -- | Creates a new map with an expected size.
   newSized :: Int -> IO (mp k v)
 
---  insert :: (Eq k, Hashable k) =>
-
   -- | Insert an entry.  This can be called from many different threads concurrently.
-  insert :: mp k v -> k -> v -> IO ()
-  -- TODO: constraint
+  insert :: (Key mp k) => mp k v -> k -> v -> IO ()
   
   -- | Looks up a key-value mapping.  Note that this may race with insert, in which
   -- case either `Nothing` or `Just` could be returned.
-  lookup :: mp k v -> k -> IO (Maybe v)
-  -- TODO: constraint
+  lookup :: (Key mp k) => mp k v -> k -> IO (Maybe v)
 
   -- foldM
   -- mapM_
   -- computeOverhead
+
 
 -- | A fully functional concurrent map that allows concurrent deletion as well as
 -- insertion.
 class ConcurrentInsertMap mp => ConcurrentMap mp where
 
   -- | Concurrent deletion of an entry in the map
-  delete :: mp k v -> k -> IO ()
-  -- TODO: constraint
+  delete :: (Key mp k) => mp k v -> k -> IO ()
