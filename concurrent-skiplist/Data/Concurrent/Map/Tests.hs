0256192cd63dc205d0414e9a3b7b79ed490872de
Ryan Newton <rrnewton@gmail.com>
2014-06-05 11:01:15 -0400

    Revert "Switch over to passing the 'new' function around rather than the ugly Phantom datatype."
    This reverts commit 96fcd74fa5cfb51740cebb027b44aa6b36140af3.

--- concurrent-skiplist/Data/Concurrent/Map/Tests.hs
+++ concurrent-skiplist/Data/Concurrent/Map/Tests.hs
@@ -5,8 +5,8 @@
 -- that satisfies the interface.
 
 module Data.Concurrent.Map.Tests
-       ( makeConcurrentInsertMapTests
-       )
+       ( Phantom(..),
+         makeConcurrentInsertMapTests)
        where
 
 import Data.Concurrent.Map.Class
@@ -16,30 +16,17 @@ import           Test.Framework.Providers.HUnit  (hUnitTestToTests)
 import           Test.HUnit as HU
 --------------------------------------------------------------------------------
 
+data Phantom (a :: * -> * -> *) = Phantom
+
 -- | Build a series of tests that exercise a concurrent map, without assuming deletion capability
---
---   By convention, this takes the "new map function" as input to disambiguate WHICH map we want to test.
-makeConcurrentInsertMapTests :: ConcurrentInsertMap mp => IO (mp k v) -> [TF.Test]
-makeConcurrentInsertMapTests newFn  =
+makeConcurrentInsertMapTests :: forall (mp :: * -> * -> *) .
+                                ConcurrentInsertMap mp => Phantom mp -> [TF.Test]
+makeConcurrentInsertMapTests (_ :: Phantom mp)  =
   []
 
 
--- test1 :: IO (String)
--- test1 = do
---   lm <- LM.newLMap
---   LM.NotFound tok <- LM.find lm 1
---   LM.tryInsert tok "Hello"
---   LM.NotFound tok <- LM.find lm 0
---   LM.tryInsert tok " World"
---   LM.Found s1 <- LM.find lm 1
---   LM.Found s0 <- LM.find lm 0
---   return $ s1 ++ s0  
-
-
---------------------------------------------------------------------------------
-
 -- | Make a test suite that exercises a concurrent map INCLUDING concurrent deletion.
 --   This includes all the tests from 
--- makeConcurrentMapTests :: ConcurrentMap mp => 
+-- makeConcurrentMapTests :: ConcurrentMap
 
 
--- concurrent-skiplist/tests/GenericTests.hs
+++ concurrent-skiplist/tests/GenericTests.hs
@@ -3,12 +3,11 @@
 
 module Main where
 
-import Data.Concurrent.Map.Class as C
+import Data.Concurrent.Map.Class
 import Data.Concurrent.Map.Tests as T
 import Data.Concurrent.SkipListMap as SLM
 
 import qualified Test.Framework as TF
 
 
-main = TF.defaultMain (T.makeConcurrentInsertMapTests (C.new :: IO (SLMap k v)))
-
+main = TF.defaultMain (T.makeConcurrentInsertMapTests (Phantom :: Phantom SLMap))
