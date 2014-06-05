96fcd74fa5cfb51740cebb027b44aa6b36140af3
Ryan Newton <rrnewton@gmail.com>
2014-06-05 10:52:41 -0400

    Switch over to passing the 'new' function around rather than the ugly Phantom datatype.

--- concurrent-skiplist/tests/GenericTests.hs
+++ concurrent-skiplist/tests/GenericTests.hs
@@ -3,11 +3,12 @@
 
 module Main where
 
-import Data.Concurrent.Map.Class
+import Data.Concurrent.Map.Class as C
 import Data.Concurrent.Map.Tests as T
 import Data.Concurrent.SkipListMap as SLM
 
 import qualified Test.Framework as TF
 
 
-main = TF.defaultMain (T.makeConcurrentInsertMapTests (Phantom :: Phantom SLMap))
+main = TF.defaultMain (T.makeConcurrentInsertMapTests (C.new :: IO (SLMap k v)))
+
